4b95ac6148644858d167dd0fa206ffbf22f1f34b
Bogdan Sadikovic <94764889+bogdansadikovic@users.noreply.github.com>
2024-11-03 01:49:36 -0400

    Update plant_classifier.py

--- plant_classifier.py
+++ plant_classifier.py
@@ -4,27 +4,46 @@ import numpy as np
 import torchvision
 import os
 import sys
-import streamlit as st
 import torchvision.transforms as transforms
 from PIL import Image
 import json
 
 
-model_path = 'mushroom_classfier.pth'
+
+# CNN WORK (BOGDAN'S PART):
+
+model_path = 'mushroom_classfier1.pth'
 
 
 class MushroomClassfier(nn.Module):
     def __init__(self):
         super(MushroomClassfier, self).__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Dropout(0.5),
         )
         self.fc_layers = nn.Sequential(
-            nn.Linear(16 * 112 * 112, 128),
+            nn.Linear(256 * 14 * 14, 512),
             nn.ReLU(),
-            nn.Linear(128, 2)
+            nn.Dropout(0.5),  
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128, 2)  
         )
 
     def forward(self, x):
@@ -45,8 +64,12 @@ def load_model(model_path):
 
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.Resize((224, 224)),  
+    # The below codes that are commented out were used for data augmentation, but I ultimately decided to take them out as it was more useful to keep the dataset normal. --> Kept for reasons of making model potetnially more accurate in future.
+    # transforms.RandomHorizontalFlip(),  # Randomly flip images horizontally
+    # transforms.RandomRotation(15),      # Rotate images by up to 15 degrees
+    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.ToTensor(),
 ])
 
 def classify(image_path, model, transform):
@@ -65,6 +88,7 @@ def classify(image_path, model, transform):
 idx_to_class = {0: 'edible', 1:'poisonous'}
 # Load the model into the application:
 model = load_model(model_path)
+
 
 print('Idenityfing mushroom:')
 image_path = 'rod_sopp.png'
